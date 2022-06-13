d9efb60d9032994e6ef2b5bcd1205a9100260ecc
kalibianw <kalibianw@gmail.com>
2022-06-13 23:10:24 +0900

    Implement plot confusion metrics method

--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ from keras import mixed_precision
 from tqdm import tqdm
 import os
 
+import itertools
+import matplotlib.pyplot as plt
+
 
 class DataModule:
     def __init__(self, root_path=None):
@@ -156,3 +159,44 @@ class TrainModule:
         )
         model.load_weights(self.CKPT_PATH)
         model.save(self.MODEL_PATH)
+
+
+def plot_confusion_matrix(cm, target_names=None, cmap=None, normalize=True, labels=True, title='Confusion matrix', fname=None):
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names)
+        plt.yticks(tick_marks, target_names)
+
+    if labels:
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            if normalize:
+                plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                         horizontalalignment="center",
+                         color="white" if cm[i, j] > thresh else "black")
+            else:
+                plt.text(j, i, "{:,}".format(cm[i, j]),
+                         horizontalalignment="center",
+                         color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    if fname is not None:
+        plt.savefig(fname, dpi=300)
+    plt.show()
