6c75b9b6760942e2122403a6723b8d4f22c04c41
Jongbin Chae <kalibianw@gmail.com>
2022-08-26 15:57:01 +0900

    Move code block to main

--- preprocessing_pt.py
+++ preprocessing_pt.py
@@ -13,45 +13,46 @@ TRAIN_CSV_EXPORT_PATH = "D:/AI/data/HAM10000/train.csv"
 TEST_CSV_EXPORT_PATH = "D:/AI/data/HAM10000/test.csv"
 TEST_SIZE = 0.3
 
-org_df = pd.read_csv(CSV_FILE_PATH)
-print(org_df)
+if __name__ == '__main__':
+    org_df = pd.read_csv(CSV_FILE_PATH)
+    print(org_df)
 
-image_paths = org_df["image_id"].to_numpy()
-labels = org_df["dx"].to_numpy()
+    image_paths = org_df["image_id"].to_numpy()
+    labels = org_df["dx"].to_numpy()
 
-ord_enc = LabelEncoder()
+    ord_enc = LabelEncoder()
 
-ros = RandomOverSampler()
-image_paths, labels = ros.fit_resample(
-    X=np.expand_dims(image_paths, axis=-1),
-    y=np.expand_dims(labels, axis=-1)
-)
+    ros = RandomOverSampler()
+    image_paths, labels = ros.fit_resample(
+        X=np.expand_dims(image_paths, axis=-1),
+        y=np.expand_dims(labels, axis=-1)
+    )
 
-labels = ord_enc.fit_transform(labels).flatten()
-print(image_paths.shape, labels.shape)
-print(type(image_paths), type(labels))
+    labels = ord_enc.fit_transform(labels).flatten()
+    print(image_paths.shape, labels.shape)
+    print(type(image_paths), type(labels))
 
-train_image_paths, test_image_paths, train_labels, test_labels = train_test_split(image_paths, labels, test_size=TEST_SIZE)
-print(
-    train_image_paths.shape,
-    test_image_paths.shape,
-    train_labels.shape,
-    test_labels.shape
-)
+    train_image_paths, test_image_paths, train_labels, test_labels = train_test_split(image_paths, labels, test_size=TEST_SIZE)
+    print(
+        train_image_paths.shape,
+        test_image_paths.shape,
+        train_labels.shape,
+        test_labels.shape
+    )
 
-train_df = pd.DataFrame(
-    {
-        "image_paths": train_image_paths.flatten(),
-        "labels": train_labels
-    },
+    train_df = pd.DataFrame(
+        {
+            "image_paths": train_image_paths.flatten(),
+            "labels": train_labels
+        },
 
-)
-test_df = pd.DataFrame(
-    {
-        "image_paths": test_image_paths.flatten(),
-        "labels": test_labels
-    }
-)
+    )
+    test_df = pd.DataFrame(
+        {
+            "image_paths": test_image_paths.flatten(),
+            "labels": test_labels
+        }
+    )
 
-train_df.to_csv(TRAIN_CSV_EXPORT_PATH, index=False)
-test_df.to_csv(TEST_CSV_EXPORT_PATH, index=False)
+    train_df.to_csv(TRAIN_CSV_EXPORT_PATH, index=False)
+    test_df.to_csv(TEST_CSV_EXPORT_PATH, index=False)
